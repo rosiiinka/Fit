f46a6843055ba9264966998965b160c555c25d4b
Rositsa Koriykova <rosi.korikova@gmail.com>
2018-04-15 17:47:16 +0300

    templates

---
+++ src/main/java/com/example/my_fit/MyFitApplication.java
@@ -0,0 +1,12 @@
+package com.example.my_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFitApplication.class, args);
+    }
+}
